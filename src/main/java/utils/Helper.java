251761309c472c45f28c92012d4fb272dfa0247c
MinhPham <mxstudios.dn@gmail.com>
2025-12-09 16:09:48 +0700

    remove unuse files

--- src/main/java/utils/Helper.java
+++ src/main/java/utils/Helper.java
@@ -1,10 +1,7 @@
 package utils;
 
-import core.TestSettings;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.nio.charset.StandardCharsets;
